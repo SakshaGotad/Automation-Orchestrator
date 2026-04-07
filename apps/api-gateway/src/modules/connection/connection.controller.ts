dcab8dd69a09796b71bcd86ec49f88e83887e981
SakshaGotad <sakshagotad@gmail.com>
2026-04-07 17:29:16 +0530

    refactor: update connection controller to support dynamic userId via query parameters

--- apps/api-gateway/src/modules/connection/connection.controller.ts
+++ apps/api-gateway/src/modules/connection/connection.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
 import { ConnectionService } from './connection.service';
 import { CreateConnectionDto } from './dto/create-connection.dto';
 
@@ -6,15 +6,9 @@ import { CreateConnectionDto } from './dto/create-connection.dto';
 export class ConnectionController {
   constructor(private readonly connectionService: ConnectionService) {}
 
-  @Post()
-  create(@Body() dto: CreateConnectionDto) {
-    const userId = 'user_1';
-    return this.connectionService.create(userId, dto.provider);
-  }
 
   @Get()
-  findAll() {
-    const userId = 'user_1';
+  findAll(@Query('userId') userId: string) {
     return this.connectionService.findAll(userId);
   }
 }
