a54646e9446d10c9aff0936c0d4b40fa9722a84e
SakshaGotad <sakshagotad@gmail.com>
2026-04-08 23:09:24 +0530

    feat: implement webhook controller and service to process incoming provider payloads

--- apps/ingestion-service/src/webhook/webhook.controller.ts
+++ apps/ingestion-service/src/webhook/webhook.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Query } from '@nestjs/common';
+import { WebhookService } from './webhook.service';
 
 @Controller('webhook')
-export class WebhookController {}
+export class WebhookController {
+    constructor (private readonly webhookService: WebhookService) {}
+
+    @Post(':provider')
+    async handleWebhook(@Param('provider') provider: string, 
+    @Query('connectionId') connectionId: string,
+    @Body() payload: any) {
+        return this.webhookService.processWebhook({ provider, connectionId, payload });
+    }
+}
--- apps/ingestion-service/src/webhook/webhook.service.ts
+++ apps/ingestion-service/src/webhook/webhook.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class WebhookService {}
+export class WebhookService {
+  async processWebhook(data: {
+    provider: string;
+    connectionId: string;
+    payload: any;
+  }) {
+    const { provider, connectionId, payload } = data;
+
+    console.log('--- Webhook Received ---');
+    console.log('Provider:', provider);
+    console.log('ConnectionId:', connectionId);
+    console.log('Payload:', payload);
+
+    // Later:
+    // 1. Validate connection
+    // 2. Match workflows
+    // 3. Send to Kafka
+
+    return {
+      message: 'Webhook received successfully',
+    };
+  }
+}
