2f67fb4603e126e9e434bb80c7cfdb958aa9d45b
SakshaGotad <sakshagotad@gmail.com>
2026-04-12 15:25:15 +0530

    feat: implement Kafka producer service and integrate event publishing into webhook processing flow

--- apps/ingestion-service/src/app.module.ts
+++ apps/ingestion-service/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebhookModule } from './webhook/webhook.module';
 import { PrismaModule } from './database/prisma.module';
+import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
-  imports: [WebhookModule, PrismaModule],
+  imports: [WebhookModule, PrismaModule, KafkaModule],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
